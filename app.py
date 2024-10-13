f030ff3ec1e17e77554f584c008397cf67156adb
sairaj rajput <sairajrajput6@gmail.com>
2024-10-13 13:46:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ pepper = ['Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy']
 potato = ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
 soyabean = ['Soybean___healthy', 'Squash___Powdery_mildew']
 strawberry = ['Strawberry___Leaf_scorch', 'Strawberry___healthy']
+tomato = ['Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy']
+
 
 
 
@@ -132,7 +134,10 @@ def predict():
 
         elif name_of_crop == 'strawberry': 
             class_labels = strawberry  
-            MODEL_PATH = 'strawberry.tflite'
+            MODEL_PATH = 'strawberry.tflite' 
+        elif name_of_crop == 'tomato': 
+            class_labels = tomato 
+            MODEL_PATH = 'tomato.tflite' 
         else:
             return jsonify({"error": "Invalid crop name provided."}), 400
 
